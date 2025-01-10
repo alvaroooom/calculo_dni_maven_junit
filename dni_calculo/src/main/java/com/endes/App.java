7d4e2dde0f6fdc9d3d88a3c3d239676df4833c83
alvaroooom <munoz.fernandez.alvaro@iescamas.es>
2025-01-10 11:14:56 +0100

    CalculoDNI realizado, faltan pruebas

--- dni_calculo/src/main/java/com/endes/App.java
+++ dni_calculo/src/main/java/com/endes/App.java
@@ -8,6 +8,14 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        ControladorDNI controlador = new ControladorDNI();
+        
+        boolean esValido1 = controlador.esValido("81440286T");
+        System.out.println("81440286T -->" + esValido1);
+        
+        boolean esValido2 = controlador.esValido("81440286R");
+        System.out.println("81440286R -->" + esValido2);
+        
+ 
     }
 }
---
+++ dni_calculo/src/test/java/com/endes/Controlador_DNITest.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package com.endes;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * 
+ */
+class Controlador_DNITest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
